c83a0bc84b828f2f3fbdc3718dcb3a14e6d88c00
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-10 16:21:07 +0100

    Start project with Spring Initializr

---
+++ src/main/java/es/fbenavente/termfrequency/termfrequency/TermFrequencyApplication.java
@@ -0,0 +1,13 @@
+package es.fbenavente.termfrequency.termfrequency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermFrequencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermFrequencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/fbenavente/termfrequency/termfrequency/TermFrequencyApplicationTests.java
@@ -0,0 +1,13 @@
+package es.fbenavente.termfrequency.termfrequency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermFrequencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
