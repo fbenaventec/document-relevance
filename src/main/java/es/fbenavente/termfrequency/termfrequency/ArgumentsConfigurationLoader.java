05d5900b2cb25295a5fbe9948a27c716d4325575
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-12 20:25:40 +0100

    Connect ArgumentsConfigurationLoader with application

--- src/main/java/es/fbenavente/termfrequency/termfrequency/ArgumentsConfigurationLoader.java
+++ src/main/java/es/fbenavente/termfrequency/termfrequency/ArgumentsConfigurationLoader.java
@@ -2,14 +2,17 @@ package es.fbenavente.termfrequency.termfrequency;
 
 import es.fbenavente.termfrequency.termfrequency.configuration.TermFrequencyConfiguration;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class ArgumentsConfigurationLoader {
     private final TermFrequencyConfiguration termFrequencyConfiguration;
 
-    public void loadFromArgs(String ... args) {
+    public void loadFromArgs(ApplicationArguments args) {
         // TODO implement to read params following specs:
         // -d|-D for directory to scan documents
         // -tt|-TT for terms
--- src/main/java/es/fbenavente/termfrequency/termfrequency/TermFrequencyApplication.java
+++ src/main/java/es/fbenavente/termfrequency/termfrequency/TermFrequencyApplication.java
@@ -3,6 +3,8 @@ package es.fbenavente.termfrequency.termfrequency;
 import es.fbenavente.termfrequency.termfrequency.configuration.TermFrequencyConfiguration;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,10 +13,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Slf4j
 @AllArgsConstructor
 @EnableScheduling
-public class TermFrequencyApplication {
+public class TermFrequencyApplication implements ApplicationRunner {
 	private final TermFrequencyConfiguration termFrequencyConfiguration;
+	private final ArgumentsConfigurationLoader argumentsConfigurationLoader;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TermFrequencyApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		argumentsConfigurationLoader.loadFromArgs(args);
 	}
 }
