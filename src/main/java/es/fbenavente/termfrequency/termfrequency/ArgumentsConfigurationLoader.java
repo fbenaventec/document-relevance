6a45941c231cc1b078083038dee82b57f58b5095
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-12 20:21:52 +0100

    Add todo for params parser

--- src/main/java/es/fbenavente/termfrequency/termfrequency/ArgumentsConfigurationLoader.java
+++ src/main/java/es/fbenavente/termfrequency/termfrequency/ArgumentsConfigurationLoader.java
@@ -10,6 +10,10 @@ public class ArgumentsConfigurationLoader {
     private final TermFrequencyConfiguration termFrequencyConfiguration;
 
     public void loadFromArgs(String ... args) {
-
+        // TODO implement to read params following specs:
+        // -d|-D for directory to scan documents
+        // -tt|-TT for terms
+        // -p|-P for interval
+        // -n|-N for elements to show
     }
 }
