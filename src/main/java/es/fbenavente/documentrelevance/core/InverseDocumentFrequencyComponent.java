c16646c697a825c4ba963ba547838af1bb0de287
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-12 21:15:50 +0100

    Remove unused methods

--- src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
+++ src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
@@ -1,15 +1,7 @@
 package es.fbenavente.documentrelevance.core;
 
-import es.fbenavente.documentrelevance.domain.DocumentRelevance;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class InverseDocumentFrequencyComponent {
-
-    public List<DocumentRelevance> ranking() {
-        return new ArrayList<>();
-    }
 }
--- src/main/java/es/fbenavente/documentrelevance/core/TermFrequencyComponent.java
+++ src/main/java/es/fbenavente/documentrelevance/core/TermFrequencyComponent.java
@@ -1,15 +1,8 @@
 package es.fbenavente.documentrelevance.core;
 
-import es.fbenavente.documentrelevance.domain.DocumentRelevance;
 import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Component
 public class TermFrequencyComponent {
 
-    public List<DocumentRelevance> evaluate() {
-        return new ArrayList<>();
-    }
 }
