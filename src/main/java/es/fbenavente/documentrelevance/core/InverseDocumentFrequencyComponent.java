ec647ba148d9fa9cf9275b147bf6ae20a9488a4d
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-17 23:05:21 +0100

    Add implementation without test because i'm not sure with the implementaion way

--- src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
+++ src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
@@ -1,16 +1,13 @@
 package es.fbenavente.documentrelevance.core;
 
 import es.fbenavente.documentrelevance.domain.Document;
-import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Component
-public class TermFrequencyComponent {
-
-    public Map<String, Map<String, Double>> calculate(List<Document> documents, List<String> terms) {
+public class InverseDocumentFrequencyComponent {
+    public Map<String, Double> calculate(List<Document> documents, List<String> terms) {
         return new HashMap<>();
     }
 }
--- src/main/java/es/fbenavente/documentrelevance/core/ReportGenerator.java
+++ src/main/java/es/fbenavente/documentrelevance/core/ReportGenerator.java
@@ -1,5 +1,6 @@
 package es.fbenavente.documentrelevance.core;
 
+import es.fbenavente.documentrelevance.configuration.DocumentRelevanceConfiguration;
 import es.fbenavente.documentrelevance.domain.Document;
 import es.fbenavente.documentrelevance.domain.DocumentRelevance;
 import es.fbenavente.documentrelevance.domain.DocumentRelevanceReport;
@@ -7,22 +8,42 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class ReportGenerator {
     private final DocumentReader documentReader;
+    private final DocumentRelevanceConfiguration documentRelevanceConfiguration;
+    private final TermFrequencyComponent termFrequencyComponent;
+    private final InverseDocumentFrequencyComponent inverseDocumentFrequencyComponent;
 
     public DocumentRelevanceReport generate() {
         List<Document> documents = documentReader.readAll();
-        List<DocumentRelevance> documentsRelevance = documents.stream()
-                .map(file -> DocumentRelevance.builder()
-                        .name(file.getName())
-                        .ranking(BigDecimal.valueOf(Math.random()))
-                        .build())
-                .collect(Collectors.toList());
+        Map<String, Double> inverseDocumentFrequencyByTerm
+                = inverseDocumentFrequencyComponent.calculate(documents, documentRelevanceConfiguration.getTerms());
+        Map<String, Map<String, Double>> termFrequenciesByDocument
+                = termFrequencyComponent.calculate(documents, documentRelevanceConfiguration.getTerms());
+        List<DocumentRelevance> documentsRelevance = new ArrayList<>();
+        for (Document document: documents) {
+            double ranking = 0d;
+            double inverseDocumentFrequency = inverseDocumentFrequencyByTerm.getOrDefault(document.getName(), 0d);
+            Map<String, Double> termFrequenciesByTerm
+                    = termFrequenciesByDocument.getOrDefault(document.getName(), new HashMap<>());
+            for (String term: documentRelevanceConfiguration.getTerms()) {
+                double termFrequency = termFrequenciesByTerm.getOrDefault(term, 0d);
+                ranking += termFrequency + inverseDocumentFrequency;
+            }
+            documentsRelevance.add(
+                    DocumentRelevance.builder()
+                            .name(document.getName())
+                            .ranking(BigDecimal.valueOf(ranking))
+                            .build()
+            );
+        }
         return DocumentRelevanceReport.builder().documents(documentsRelevance).build();
     }
 }
