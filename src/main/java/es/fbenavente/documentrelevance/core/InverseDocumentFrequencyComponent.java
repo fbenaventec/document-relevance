727638fb4d32f81be6d30969fb0cc49c22127c76
Felipe Benavente <fbenavente@activitiesbank.com>
2019-11-17 23:06:52 +0100

    Execution error

--- src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
+++ src/main/java/es/fbenavente/documentrelevance/core/InverseDocumentFrequencyComponent.java
@@ -1,11 +1,13 @@
 package es.fbenavente.documentrelevance.core;
 
 import es.fbenavente.documentrelevance.domain.Document;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class InverseDocumentFrequencyComponent {
     public Map<String, Double> calculate(List<Document> documents, List<String> terms) {
         return new HashMap<>();
